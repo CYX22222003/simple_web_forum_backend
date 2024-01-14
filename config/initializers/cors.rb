5e31f42fbfba461162d445cdfdc03780c00a515b
CYX22222003 <e1155533@u.nus.edu>
2024-01-14 10:47:05 +0800

    update cors document

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://cyx22222003.github.io/simple_web_forum/"
 
     resource "*",
       headers: :any,
