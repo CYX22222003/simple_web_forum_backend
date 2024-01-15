32393bed1204c451f2cae7dde1facdf553c5cfaa
CYX22222003 <e1155533@u.nus.edu>
2024-01-15 15:03:45 +0800

    testing for new features

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :email
+  belongs_to :tag
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_101557) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_15_065301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_101557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "email_id"
+    t.bigint "tag_id", null: false
     t.index ["email_id"], name: "index_articles_on_email_id"
+    t.index ["tag_id"], name: "index_articles_on_tag_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -39,6 +41,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_101557) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name"
+    t.string "string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "passwd"
     t.string "username"
@@ -49,6 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_101557) do
   end
 
   add_foreign_key "articles", "emails"
+  add_foreign_key "articles", "tags"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "emails"
   add_foreign_key "users", "emails"
