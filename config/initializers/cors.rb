533509e0724475aa5b4642b2eab72930459dc881
CYX22222003 <e1155533@u.nus.edu>
2024-01-25 09:04:13 +0800

    add cors feature

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://netlify--reliable-flan-2037a9.netlify.app/"
 
     resource "*",
       headers: :any,
